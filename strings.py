5ec24609e5c18b4933a5376f5ff3e4b572320150
TapauServer <106005322+TapauServer@users.noreply.github.com>
2022-09-01 19:31:56 +0800

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+#print string
+print("Hello World") #doublecode
+print('Hello World') #singlecode
+
+#multiplelines
+print("""This string runs
+multiple lines""")
+
+#addstring
+print("This string is "+"awesome!")

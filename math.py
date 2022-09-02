f47dc39d64963083162f42d5b54d9e97f97efbf9
TapauServer <106005322+TapauServer@users.noreply.github.com>
2022-09-02 11:48:53 +0800

    Add files via upload

---
+++ math.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+#math
+
+print(50 + 50) #tambah
+
+print(50 - 50) #tolak
+
+print(50 * 50) #darab
+
+print(50 / 50) #bahagi
+
+print(50 + 50 - 50 * 50 / 50) #all combine
+
+print(50 ** 2) #exponents/kuasa
+
+print(50 % 6) #modulo
+
+print(50 / 6) #bahagi
+print(50 // 6) #no leftovers
